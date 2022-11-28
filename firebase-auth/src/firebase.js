9ac65de9eee4318d4d4a65fa0f81bfda65c8cea0
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-28 14:41:32 +0200

    needs check sign up

--- firebase-auth/src/firebase.js
+++ firebase-auth/src/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
+import firebase from "firebase/compat/app";
 
-import "firebase/auth";
+import "firebase/compat/auth";
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
