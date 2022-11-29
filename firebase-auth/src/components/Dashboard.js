f51fe4629a797e2f2c91fc2a5f2880a34bcd4cdf
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-29 15:14:44 +0200

    logout functionality

--- firebase-auth/src/components/Dashboard.js
+++ firebase-auth/src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Card, Button, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   const [error, setError] = useState("");
@@ -13,6 +14,9 @@ export default function Dashboard() {
           <h2 className="text-center mb-4">Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <strong>Email : </strong> {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
